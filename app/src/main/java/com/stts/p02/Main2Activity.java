0314c585d7823b28913c842e2d16c7b61d38a5a8
ProtoZxn99 <patrick.soebiantoro@gmail.com>
2019-02-12 13:01:56 +0700

    i

--- app/src/main/java/com/stts/p02/Main2Activity.java
+++ app/src/main/java/com/stts/p02/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.stts.p02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent i = getIntent();
+        String user = i.getStringExtra("user");
+        String pass = i.getStringExtra("pass");
+
+        ((TextView)findViewById(R.id.textView3)).setText(user+" "+pass);
     }
 }
--- app/src/main/java/com/stts/p02/MainActivity.java
+++ app/src/main/java/com/stts/p02/MainActivity.java
@@ -1,13 +1,38 @@
 package com.stts.p02;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtuser, txtpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtuser = findViewById(R.id.editText);
+        txtpass = findViewById(R.id.editText2);
+    }
+
+    public void btnLoginClick(View v){
+        Intent i = new Intent(this,Main2Activity.class);
+        i.putExtra("user",txtuser.getText().toString());
+        i.putExtra("pass",txtpass.getText().toString());
+        startActivity(i);
+    }
+
+    public void btnSendClick(View v){
+        Uri u = Uri.parse("smsto:08991269638");
+        Intent i = new Intent(Intent.ACTION_SENDTO, u);
+        i.putExtra("sms_body",(new Random().nextInt(1000))+1000+"");
+        startActivity(i);
     }
 }
